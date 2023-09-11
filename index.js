6309f89a4449f099276b0fbb114535e50d606ef3
Moraru-John <138571560+Moraru-John@users.noreply.github.com>
2023-09-11 14:31:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-function toggleModeNav() {
-    const nav = document.getElementById('nav');
-    nav.classList.toggle("dark-nav");
-}    
 const currentMode = nav.classList.contains("dark-nav") ? "dark" : "light";
 localStorage.setItem("mode", currentMode);
 
 
 const storedMode = localStorage.getItem("mode");
 if (storedMode === "dark") {
-    toggleModeNav();
+    toggleMode();
 }
 
-function toggleModeMain() {
+function toggleMode() {
+  const nav = document.getElementById('nav');
   const main = document.getElementById('main');
   const header = document.getElementById('header');
-  const footer = document.getElementById('footer')
+  const footer = document.getElementById('footer');
+  nav.classList.toggle("dark-nav");
   main.classList.toggle("dark-hmf");
   header.classList.toggle("dark-hmf");
   footer.classList.toggle("dark-hmf");
 }
 
 const modeToggleBtn = document.getElementById("mode-toggle");
-modeToggleBtn.addEventListener("click", toggleModeNav);
-modeToggleBtn.addEventListener("click", toggleModeMain);
+modeToggleBtn.addEventListener("click", toggleMode);
